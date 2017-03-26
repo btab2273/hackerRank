c6a4e7aff140ede9d524ba41b99666687d05fa0f
BT <tabengwa@gmail.com>
2017-03-26 17:05:37 -0500

    second challenge

---
+++ 30day_challenge/day01_data_types.py
@@ -0,0 +1,24 @@
+import sys
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables
+i2 = int(input())
+
+d2 = float(input())
+
+s2 = str(input())
+
+# Print the sum of both integer variables
+sum_both = i + i2
+print(sum_both)
+
+# Print the sum of both the double variables
+sum_double = d + d2
+print(sum_double)
+
+# Concatenate and print the string variables
+con_strings = s + s2
+print(con_strings)
