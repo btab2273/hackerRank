d18e24740a553402a2e9a20fc81fe9f29ad18209
BT <btab2273@users.noreply.github.com>
2017-04-03 16:00:48 -0500

    Create simple_sum.py

---
+++ simple_sum.py
@@ -0,0 +1,11 @@
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+# Print the sum of the array's elements: array_sum
+array_sum  = sum(arr)
+
+print(array_sum)
